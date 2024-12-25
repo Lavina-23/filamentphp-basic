92b6386fe0f5c15b1fa178f56e4397ef07557c90
Lavina-23 <lavinakohai3227@gmail.com>
2024-12-25 16:51:24 +0700

    feat: handle is active toggle on create and edit

--- app/Filament/Resources/HeroResource/Pages/EditHero.php
+++ app/Filament/Resources/HeroResource/Pages/EditHero.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\HeroResource\Pages;
 use App\Filament\Resources\HeroResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditHero extends EditRecord
 {
@@ -20,5 +21,15 @@ class EditHero extends EditRecord
     protected function getRedirectUrl(): string
     {
         return static::getResource()::getUrl('index');
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record = parent::handleRecordUpdate($record, $data);
+        if ($record->isActive) {
+            HeroResource::getModel()::where('id', '!=', $record->id)->update(['isActive' => false]);
+        }
+
+        return $record;
     }
 }
